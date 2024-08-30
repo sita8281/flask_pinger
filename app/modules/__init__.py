bfb4ece67b32e996ebffc3276101410769d40729
artem8281 <mrsit8281@yandex.ru>
2024-08-30 22:55:45 +0300

    [fix] modules/__init__

--- app/modules/__init__.py
+++ app/modules/__init__.py
@@ -1,5 +1,9 @@
 from .icmp_daemon import ICMPdaemon
+from .web_session_maker import SessionMaker
 
 
+sessions_online = SessionMaker()
 pinger = ICMPdaemon()
+
 pinger.start()
+sessions_online.start()
